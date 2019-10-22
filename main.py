b46cedbfaef83082ece38a7109dfd7b017844af5
Alpesh Patel <alpeshmpatel@gmail.com>
2019-10-22 18:59:48 +0100

    Create help command

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from show_help import ShowHelp
+
 def run_command(command):
     if command.startswith("--help"):
-        pass
+        showhelp = ShowHelp()
+        showhelp.show()
     elif command.startswith("--list"):
         pass
     elif command == "--exit":
---
+++ show_help.py
@@ -0,0 +1,9 @@
+
+class ShowHelp:
+    def show(self):
+        print("*" * 48)
+        print("-" * 10 + " Help Options:" + "-" * 10)
+        print("--help       Show this screen.")
+        print("--list       List the top 5 items from sorted dataset.")
+        print("--exit       Exit.")
+        print("\n")
