2387f75d302312f2a7467b8f0dbd978760d56131
Alpesh Patel <alpeshmpatel@gmail.com>
2019-10-22 19:46:16 +0100

    Tenant and mask count report

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
+from mast_stat import MastStat
 from mast_data_reader import MastDataReader
 from show_help import ShowHelp
 import datetime
 from datetime import timedelta
 
+path_to_csv = "Mobile Phone Masts.csv"
+
 def print_list(list_to_print):
     for item in list_to_print:
         print(item)
+
+def print_dict(dict_to_print):
+    print("*" * 87)
+    print("| Tenant Name" + " " * 60 +"| Total Mast |")
+    print("*" * 87)
+    for k, v in dict_to_print.items():
+        print("|{:55}                 |{:12}|".format(k,v))
 
 def run_command(command):
     if command.startswith("--help"):
         showhelp = ShowHelp()
         showhelp.show()
     elif command.startswith("--list"):
-        mastreader = MastDataReader("Mobile Phone Masts.csv")
+        mastreader = MastDataReader(path_to_csv)
         top5 = mastreader.sorted_top(5)
         print_list(top5)
+    elif command.startswith("--filter"):
+        mastreader = MastDataReader(path_to_csv)
+        lease25year = mastreader.list_filtered(lambda x: x[9] == "25")
+        print_list(lease25year)
+    elif command.startswith("--stat"):
+        maststate = MastStat()
+        stat = maststate.get_tenant_mast_count(MastDataReader(path_to_csv).get_all(), 6)
+        print_dict(stat)
     elif command == "--exit":
         pass
+
 
 command = input()
 run_command(command)
--- mast_data_reader.py
+++ mast_data_reader.py
@@ -3,10 +3,23 @@ import csv
 class MastDataReader:
     def __init__(self, path_to_csv):
         self.path_to_csv = path_to_csv
-    
+
+    def get_all(self):
+        with open(self.path_to_csv, newline='') as fd:
+            csvReader = csv.reader(fd, delimiter = ',')
+            next(csvReader)
+            return list(csvReader)
+   
     def top(self, n):
         with open(self.path_to_csv, newline='') as fd:
             csvReader = csv.reader(fd, delimiter = ',')
             next(csvReader)
             sortedList = list(sorted(csvReader, key=lambda row: float(row[10]), reverse=False))
             return sortedList[0:n]
+
+    def list_filtered(self, filter_expr):
+        with open(self.path_to_csv, newline='') as fd:
+            csvReader = csv.reader(fd, delimiter = ',')
+            next(csvReader)
+            filteredList = list(filter(filter_expr, csvReader))
+            return filteredList
